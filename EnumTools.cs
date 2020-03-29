c49990a71fee70b634e347f8b5241d3253e7acd0
Pavel <pavel.suvorov@kosmos.games>
2020-03-29 18:18:32 +0300

    enum tools

---
+++ EnumTools.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Ps.Utils
+{
+    public static class EnumTools
+    {
+        public static bool Includes<T>(this T mask, T flags) where T : Enum
+        {
+            int maskValue = (int) (object) mask;
+            int flagsValue = (int) (object) flags;
+            return (maskValue & flagsValue) == flagsValue;
+        }
+
+        public static bool Crosses<T>(this T mask, T flags) where T : Enum
+        {
+            int maskValue = (int) (object) mask;
+            int flagsValue = (int) (object) flags;
+            return (maskValue & flagsValue) != 0;
+        }
+    }
+}
