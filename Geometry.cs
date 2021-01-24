9757fad4ec7c7000a22ac15e8be0ce5ead078495
Pavel <pavel.suvorov@kosmos.games>
2021-01-24 23:36:32 +0300

    geometry tools

---
+++ Geometry.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Lib.UnityQuickTools
+{
+    
+    public static class Geometry
+    {
+        public static Vector3 GetRandomForward() => Quaternion.Euler(0, Random.Range(0, 360), 0) * Vector3.forward;
+    }
+    
+    
+}
